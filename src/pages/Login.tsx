eb914b70aac1251a2805b9e61aa8836cac9b7d90
Baidyanath-k <baidyanathkumar12091998@gmail.com>
2024-07-10 11:45:48 +0600

    token verify

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,19 +1,29 @@
 import { useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
+import { setUser } from "../redux/features/auth/authSlice";
+import { useAppDispatch } from "../redux/hooks";
+import { tokenVerify } from "../utils/tokenVerify";
 
 const Login = () => {
   const { register, handleSubmit } = useForm();
+
+  const dispatch = useAppDispatch();
 
   const [login, { data, error }] = useLoginMutation();
   console.log(data);
   console.log(error);
 
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     const userInfo = {
       id: data.id,
       password: data.password,
     };
-    login(userInfo);
+    const res = await login(userInfo).unwrap();
+
+    const user = tokenVerify(res.data.accessToken);
+    console.log(user);
+
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
   };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
